497a9135cbc249c340cfb0ef4aa44e88fbe80909
marasoares <89050695+marasoares@users.noreply.github.com>
2021-11-30 23:36:39 -0300

    Resource filmes

--- src/filmes/filmes.controller.ts
+++ src/filmes/filmes.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { FilmesService } from './filmes.service';
 import { CreateFilmeDto } from './dto/create-filme.dto';
 import { UpdateFilmeDto } from './dto/update-filme.dto';
